f816705ad9f7b22fce462d0c2590f3b3bd434f7a
JFoll0823 <justinfoll23@gmail.com>
2021-11-11 18:45:03 -0500

    Jump and Gravity implemented

--- ChicosLastStand/Assets/Player/ThirdPersonMovement.cs
+++ ChicosLastStand/Assets/Player/ThirdPersonMovement.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//tutorial for this code and camera setup https://www.youtube.com/watch?v=4HpC--2iowE
+//tutorial for this code and camera setup - Third Person Movment Video: https://www.youtube.com/watch?v=4HpC--2iowE
+//tutorial for gravity code - First Person Movement Video: https://youtu.be/_QajrabyTJc
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     public float speed = 6f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    Vector3 velocity;
+
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -32,5 +50,15 @@ public class ThirdPersonMovement : MonoBehaviour
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") &&  isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //First Person Movement video
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
