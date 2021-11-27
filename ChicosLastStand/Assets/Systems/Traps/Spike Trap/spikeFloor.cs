41933a3b613cf808672e2d5000abad14ea0f2bd6
JFoll0823 <justinfoll23@gmail.com>
2021-11-27 14:14:51 -0500

    Spike Trap Improvements
    Spikes translate up over short period
    Spikes dont damage until they have quit moving

--- ChicosLastStand/Assets/Systems/Traps/Spike Trap/spikeFloor.cs
+++ ChicosLastStand/Assets/Systems/Traps/Spike Trap/spikeFloor.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class spikeFloor : MonoBehaviour
 {
     [SerializeField] GameObject spikes;
+    Vector3 aPos;
+
+    //spike rise vars
+    bool isTriggering;
+    float start;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +20,31 @@ public class spikeFloor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //spikes rise on trigger
+        if (isTriggering && (Time.time - start > .75))
+        {
+            if (spikes.transform.position.y <= aPos.y + 1)
+            {
+                spikes.transform.Translate(0, Time.deltaTime * 10, 0);
+            }
+            else
+            {
+                isTriggering = false;
+                spikes.GetComponent<BoxCollider>().enabled = true;
+            }
+            
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Zombie")
         {
-            Debug.Log("triggered");
-            spikes.transform.position += new Vector3(0, 1, 0);
+            //Debug.Log("triggered");
+            aPos = spikes.transform.position;
+            isTriggering = true;
+            start = Time.time;
+            //spikes.transform.position += new Vector3(0, 1, 0);
             gameObject.GetComponent<BoxCollider>().enabled = false;
         }
     }
